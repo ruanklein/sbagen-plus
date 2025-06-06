127252619e5227bda6822772c3a3400540269db7
Ruan <ruan.klein@gmail.com>
2025-06-05 22:13:22 -0300

    chore: option added to include sbagen+ in Windows PATH

--- windows-create-installer.sh
+++ windows-create-installer.sh
@@ -33,7 +33,7 @@ XVFB_PID=$(pgrep -f "Xvfb $DISPLAY -screen 0 1024x768x16")
 # Start Xvfb to provide a virtual display if it's not already running
 if [ -z "$XVFB_PID" ]; then
     info "Starting Xvfb for headless Wine operation..."
-    Xvfb $DISPLAY -screen 0 1024x768x16 & XVFB_PID=$!
+    Xvfb $DISPLAY -screen 0 1024x768x16 & XVFB_PID=$! > /dev/null 2>&1 || true
     sleep 2  # Wait for Xvfb to start
 fi
 
